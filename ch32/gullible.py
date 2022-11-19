9136562a12c3c30c9abdc8a61a7b74b5e52769b9
Jaecobd1 <jaecob.d1@gmail.com>
2022-11-18 19:43:16 -0500

    adding ch32

---
+++ ch32/gullible.py
@@ -0,0 +1,14 @@
+"""Gullible, by Jake Dobler jaecob.d1@gmail.com"""
+
+print('Gullible, by Jake Dobler jaecob.d1@gmail.com')
+
+while True: # Main program loop.
+    print("Do you want to know how to keep a gullible person busy for hours? Y/N")
+    response = input('> ') # Get the user's response.
+    if response.lower() == 'no' or response.lower() =='n':
+        break # If "no", break out of this loop.
+    if response.lower() == 'yes' or response.lower() == 'y':
+        continue # If "yes", continue to the start of this loop
+    print('"{}" is not a valid yes/no response.'.format(response))
+
+print('Thank you. Have a nice day!')
